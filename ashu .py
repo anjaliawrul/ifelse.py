ce1e7e6b79e5bd15c1a02edc92310e39a6179ef3
anjaliawrul <111339784+anjaliawrul@users.noreply.github.com>
2022-09-15 14:15:23 +0530

    Add files via upload

---
+++ ashu .py
@@ -0,0 +1,25 @@
+# a=(input("enter the name of country"))
+# b=int(input("enter the name of age"))
+# c=(input("enter the gender"))
+# if a=="india":
+#     if b==18:
+#         if c=="female" or c=="male":
+#             print("you can stand for political")
+#         else:
+#             print("you can't stand for political")
+# elif a=="american":
+#     if b==35:
+#         if c=="female" or "male":
+#             print("you can live in india")
+#         else:
+#             print("you can't live in india")
+# elif a=="russia":
+#     if b==40:
+#         if c=="female" or "male":
+#             print("you are the friend of india")
+#         else:
+#             print("you are not the friend of india")
+# else:
+#     print("permentally you can becomes indias and u will be safe ")
+            
+
